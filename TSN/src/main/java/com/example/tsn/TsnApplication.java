ad6aca87b1bf64fee6adde61659feb5e627c18af
zelinze <112954210+zelinze@users.noreply.github.com>
2024-04-24 16:47:34 +0200

    initial

---
+++ TSN/src/main/java/com/example/tsn/TsnApplication.java
@@ -0,0 +1,13 @@
+package com.example.tsn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TsnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TsnApplication.class, args);
+    }
+
+}
---
+++ TSN/src/test/java/com/example/tsn/TsnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tsn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TsnApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
